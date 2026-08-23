ba311d023600420ccc13a11105baccc1b7d1c46c
Amjad Saidam <amjadsaidama@gmail.com>
2026-08-23 17:52:05 +0100

    fix: absolute python interprete executable path

--- run_strategy.bat
+++ run_strategy.bat
@@ -1,5 +1,5 @@
 @echo off
 :loop
-".\.venv\Scripts\python.exe" "C:\FTMO_Live_Algo\live_trading\live_loop.py"
+"C:\FTMO_Live_Algo\.venv\Scripts\python.exe" "C:\FTMO_Live_Algo\live_trading\live_loop.py"
 timeout /t 5
 goto loop
