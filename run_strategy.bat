44ee5218a9d2f0d2c0f4ac9e835c37bd1e3a5db0
Amjad Saidam <amjadsaidama@gmail.com>
2026-08-31 14:50:15 +0100

    feat: updated batch file to print crash logs

--- run_strategy.bat
+++ run_strategy.bat
@@ -1,5 +1,5 @@
 @echo off
 :loop
-"C:\FTMO_Live_Algo\.venv\Scripts\python.exe" "C:\FTMO_Live_Algo\live_trading\live_loop.py"
+"C:\FTMO_Live_Algo\.venv\Scripts\python.exe" "C:\FTMO_Live_Algo\live_trading\live_loop.py" >> "C:\FTMO_Live_Algo\logs\task_stdout.log" 2>&1
 timeout /t 5
 goto loop
